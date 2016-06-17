1ada21bb1f502f3fe90c59dd3e2704199e7d47b7
Katja Maenpaa <t-kamaen@mds.microsoft.com>
2016-06-17 08:42:41 +0300

    Lisätty kartta, kävijälaskuri ja google analytics koodi

--- script.js
+++ script.js
@@ -13,6 +13,22 @@ var ika = document.lomake.ika.value;
         alert("Toimii");
 }
 
+function initMap() {
+    var myLatLng = {lat: 60.804167, lng: 23.486111};
+    var mapDiv = document.getElementById('map');
+     var map = new google.maps.Map(mapDiv, {
+        center: myLatLng,
+        zoom: 10
+    });
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+        map: map,
+        position: myLatLng,
+        title: 'Jokioinen'
+    });
+}
+
+
 /*var linkin_nimi = "Ohjelmointiputkaan";
 document.write(linkin_nimi.link("http://www.ohjelmointiputka.net") + "<br>");
 
